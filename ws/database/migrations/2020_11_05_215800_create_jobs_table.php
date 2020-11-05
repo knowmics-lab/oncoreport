b412c63666eb7f430255189a8d897b4aa12eb083
Salvatore Alaimo <alaimos@gmail.com>
2020-11-05 23:09:19 +0100

    Bugfixes on app docker container. Creating app web service

---
+++ ws/database/migrations/2020_11_05_215800_create_jobs_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'jobs',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('sample_code')->nullable()->index();
+                $table->string('name')->nullable()->default(null);
+                $table->string('job_type');
+                $table->enum('status', ['ready', 'queued', 'processing', 'completed', 'failed'])->default('ready');
+                $table->json('job_parameters');
+                $table->json('job_output');
+                $table->longText('log');
+                $table->unsignedBigInteger('patient_id')->nullable()->index();
+                $table->foreign('patient_id', 'patient_id_to_patient_foreign_key')->references('id')->on('patients')
+                      ->onDelete('set null')->onUpdate('set null');
+                $table->unsignedBigInteger('user_id')->index();
+                $table->foreign('user_id', 'user_id_to_user_foreign_key')->references('id')->on('users')
+                      ->onDelete('cascade')->onUpdate('cascade');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
