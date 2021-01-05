7e23e2634c9b1eb98256daabc3a0630adbe09ab7
Salvatore Alaimo <alaimos@gmail.com>
2021-01-05 23:47:39 +0100

    Fixes to container build scripts

--- scripts/app/container/build.sh
+++ scripts/app/container/build.sh
@@ -8,6 +8,8 @@ tar -zcvf ../../ws.tgz ws/
 cd ../../ || exit
 rm -rf repo/ || exit
 
+[[ ! -f ws.tgz ]] && echo "Archive not built!" && exit
+
 docker build -t alaimos/oncoreport:v0.0.1 .
 
 rm ws.tgz
