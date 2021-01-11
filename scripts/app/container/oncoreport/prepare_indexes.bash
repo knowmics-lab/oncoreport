374db90d001fbd714a8f45ded592a24a25040fa0
Salvatore Alaimo <alaimos@gmail.com>
2021-01-11 20:45:44 +0100

    Fixes

--- scripts/app/container/oncoreport/prepare_indexes.bash
+++ scripts/app/container/oncoreport/prepare_indexes.bash
@@ -23,10 +23,10 @@ OLD_PWD=$(pwd)
 cd "$ONCOREPORT_INDEXES_PATH" || exit 101
 cleanup
 echo "Downloading hg19 bowtie index..."
-wget "https://genome-idx.s3.amazonaws.com/bt/hg19.zip" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download index" 102
+wget --progress=bar:force:noscroll "https://genome-idx.s3.amazonaws.com/bt/hg19.zip" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download index" 102
 unzip "$ONCOREPORT_INDEXES_PATH/hg19.zip" || exit_abnormal "Unable to extract index" 103
 echo "Downloading hg19 sequence..."
-wget "http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download sequence" 104
+wget --progress=bar:force:noscroll "http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download sequence" 104
 gunzip "$ONCOREPORT_INDEXES_PATH/hg19.fa.gz" || exit_abnormal "Unable to extract sequence" 105
 echo "Building hg19 sequence dictionary..."
 java -jar "$PICARD_PATH" CreateSequenceDictionary REFERENCE="$ONCOREPORT_INDEXES_PATH/hg19.fa" OUTPUT="$ONCOREPORT_INDEXES_PATH/hg19.dict" || exit_abnormal "Unable to build dictionary" 106
@@ -34,10 +34,10 @@ echo "Building hg19 sequence samtools index..."
 samtools faidx "$ONCOREPORT_INDEXES_PATH/hg19.fa" || exit_abnormal "Unable to build index" 107
 echo "Downloading hg38 bowtie index..."
 # TODO https://genome-idx.s3.amazonaws.com/bt/GRCh38_noalt_as.zip
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download index" 102
+wget --progress=bar:force:noscroll "ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz" -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download index" 102
 tar -zxvf "$ONCOREPORT_INDEXES_PATH/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz" || exit_abnormal "Unable to extract index" 103
 echo "Downloading hg38 sequence..."
-wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download sequence" 104
+wget --progress=bar:force:noscroll http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P "$ONCOREPORT_INDEXES_PATH" || exit_abnormal "Unable to download sequence" 104
 gunzip "$ONCOREPORT_INDEXES_PATH/hg38.fa.gz" || exit_abnormal "Unable to extract sequence" 105
 echo "Renaming files..."
 rename -d 's/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index/hg38/g' "$ONCOREPORT_INDEXES_PATH"/* || exit_abnormal "Unable to rename files" 108
