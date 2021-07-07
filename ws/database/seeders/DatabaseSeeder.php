4279889b99dbb4debdc6f03b45b55b63815388fb
rickrk4 <rickvrk4@gmail.com>
2021-07-07 16:02:11 +0200

    first commit

--- ws/database/seeders/DatabaseSeeder.php
+++ ws/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call(LocationSeeder::class);
 
         //Test
-        $this->call(PatientSeeder::class);
+        // $this->call(PatientSeeder::class);
     }
 }
