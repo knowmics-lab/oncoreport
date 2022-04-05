75b66789132e7a92f08aa6de193fcbd5ce0d1810
Salvatore Alaimo <alaimos@gmail.com>
2022-04-05 14:27:45 +0200

    fixes #20: better scrolling handling in large tables

--- scripts/app/container/html_source/report/assets/js/scroll.js
+++ scripts/app/container/html_source/report/assets/js/scroll.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const navbar = document.querySelector('header.navbar');
+
 const scrollListener = (e) => {
   const container = e.target.closest('.table-responsive-container');
   if (e.target.scrollLeft > 0) {
@@ -14,15 +16,44 @@ const scrollListener = (e) => {
   }
 };
 
+function scrollElement (scrollable, direction) {
+  scrollable.scroll({
+    left: scrollable.scrollLeft + direction * 150,
+    behavior: 'smooth',
+  });
+}
+
+function createScrollButtons (scrollable) {
+  const buttonStart = document.createElement('button');
+  buttonStart.classList.add('scroll-indicator-start');
+  buttonStart.innerHTML = '&lArr;';
+  buttonStart.addEventListener('click', () => scrollElement(scrollable, -1));
+  const buttonEnd = document.createElement('button');
+  buttonEnd.classList.add('scroll-indicator-end');
+  buttonEnd.innerHTML = '&rArr;';
+  buttonEnd.addEventListener('click', () => scrollElement(scrollable, +1));
+  const container = document.createElement('div');
+  container.classList.add('table-scroll-indicator');
+  container.appendChild(buttonStart);
+  container.appendChild(buttonEnd);
+  return container;
+}
+
 const debouncedResize = debounce((e) => {
   const container = e.target.closest('.table-responsive-container');
-  e.target.removeEventListener("scroll", scrollListener);
+  e.target.removeEventListener('scroll', scrollListener);
   if (e.target.clientWidth < e.target.scrollWidth) {
+    if (!container.querySelector('.table-scroll-indicator')) {
+      container.prepend(createScrollButtons(e.target));
+    }
     container.classList.add('with-scroll');
+    container.style.setProperty('--header-size', `${navbar.getBoundingClientRect().height}`);
     container.style.setProperty('--end-opacity', '1');
     container.style.setProperty('--scroll-height', `${e.target.offsetHeight - e.target.clientHeight}`);
-    e.target.addEventListener("scroll", scrollListener);
+    e.target.addEventListener('scroll', scrollListener);
   } else {
+    const scrollIndicator = container.querySelector('.table-scroll-indicator');
+    if (scrollIndicator) scrollIndicator.remove();
     container.classList.remove('with-scroll');
     container.style.setProperty('--start-opacity', '0');
     container.style.setProperty('--end-opacity', '0');
@@ -30,11 +61,11 @@ const debouncedResize = debounce((e) => {
 }, 100);
 
 const resizeObserver = new ResizeObserver(entries => {
-  entries.forEach(debouncedResize)
+  entries.forEach(debouncedResize);
 });
 
 [...document.querySelectorAll('.table-responsive-container > .table-responsive')].forEach(
   (container) => {
     resizeObserver.observe(container);
-  }
+  },
 );
--- scripts/app/container/html_source/report/assets/js/searchable.js
+++ scripts/app/container/html_source/report/assets/js/searchable.js
@@ -1,24 +1,26 @@
 'use strict';
 
-function makeSearchable (inputSelector, tableSelector) {
-  const searchInput = document.querySelector(inputSelector);
-  const searchTable = document.querySelector(tableSelector);
-  const searchTableBody = searchTable ? searchTable.querySelector('tbody') : null;
-
-  if (searchInput && searchTableBody) {
-    const debouncedSearch = debounce((value) => {
-      const searchValue = value.trim().toUpperCase();
-      const rows = [...searchTableBody.querySelectorAll('tr')];
-      rows.forEach((r) => r.classList.remove('d-none'));
-      if (searchValue !== '') {
-        rows.filter((r) =>
-          [...r.querySelectorAll('td')].every(
-            (td) => !td.textContent.toUpperCase().includes(searchValue)),
-        ).forEach((r) => r.classList.add('d-none'));
-      }
-    });
-    searchInput.addEventListener('input', (e) => debouncedSearch(e.target.value));
-  }
+function makeSearchable (tableContainer) {
+  const table = tableContainer.querySelector('table.table');
+  if (!tableContainer || !table) return;
+  const parentElement = tableContainer.parentElement.classList.contains('table-responsive-container')
+    ? tableContainer.parentElement
+    : tableContainer;
+  const searchTableBody = table.querySelector('tbody');
+  if (!searchTableBody) return;
+  const debouncedSearch = debounce((value) => {
+    const searchValue = value.trim().toUpperCase();
+    const rows = [...searchTableBody.querySelectorAll('tr')];
+    rows.forEach((r) => r.classList.remove('d-none'));
+    if (searchValue !== '') {
+      const tdSearchCallback = (td) => !td.textContent.toUpperCase().includes(searchValue);
+      rows.
+        filter((r) => [...r.querySelectorAll('td')].every(tdSearchCallback)).
+        forEach((r) => r.classList.add('d-none'));
+    }
+  });
+  const searchBox = createSearchBox((e) => debouncedSearch(e.target.value));
+  parentElement.before(searchBox);
 }
 
 function debounce (cb, delay = 200) {
@@ -32,6 +34,28 @@ function debounce (cb, delay = 200) {
   };
 }
 
-makeSearchable('[data-search-input]', '[data-search-table-container] > table.table');
-makeSearchable('[data-search-input-clinical]', '[data-search-table-container-clinical] > table.table');
-makeSearchable('[data-search-input-others]', '[data-search-table-container-others] > table.table');
+function createSearchBox (inputCallback) {
+  const searchInput = document.createElement('input');
+  searchInput.classList.add('form-control', 'w-100');
+  searchInput.style.paddingLeft = '2.75rem';
+  searchInput.placeholder = 'Enter a text to search the table...';
+  searchInput.ariaLabel = 'Enter a text to search the table...';
+  searchInput.addEventListener('input', inputCallback);
+  const searchInputContainer = document.createElement('div');
+  searchInputContainer.classList.add('position-relative');
+  searchInputContainer.innerHTML =
+    `<span class="position-absolute" style="top: 0.50rem; left: 1rem;">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Search">
+            <use xlink:href="#search-icon"/>
+        </svg>
+    </span>`;
+  searchInputContainer.append(searchInput);
+  const searchFlexContainer = document.createElement('div');
+  searchFlexContainer.classList.add(
+    'mb-2', 'd-flex', 'flex-row-reverse', 'justify-content-between', 'align-items-center',
+  );
+  searchFlexContainer.append(searchInputContainer);
+  return searchFlexContainer;
+}
+
+document.querySelectorAll('[data-searchable]').forEach(makeSearchable);
