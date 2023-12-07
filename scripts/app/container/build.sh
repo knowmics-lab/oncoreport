ccd4c3945b30820fe4f1a7c5c53e8b864d346782
Salvatore Alaimo <alaimos@gmail.com>
2023-12-07 09:40:53 +0100

    Two stage build: preparing for first stage test

--- scripts/app/container/build.sh
+++ scripts/app/container/build.sh
@@ -16,6 +16,7 @@ if [ -z "$GLTRANSLATE_JSON_PATH" ]; then
   echo "Usage: $0 [-b <branch>] [-t <gl_translate_api.json>]" && exit 1
 fi
 ONCOREPORT_BRANCH=${ONCOREPORT_BRANCH:-"master"}
+ONCOREPORT_ORIGIN=${ONCOREPORT_ORIGIN:-"https://github.com/knowmics-lab/oncoreport.git"}
 
 LATEST_GIT_TAG="$(git describe --tags $(git rev-list --tags --max-count=1) | cut -d'-' -f1)"
 DEFAULT_VERSION="v${LATEST_GIT_TAG:-"0.0.1"}"
@@ -23,6 +24,7 @@ DEFAULT_VERSION="v${LATEST_GIT_TAG:-"0.0.1"}"
 # Container name and version can be set with environment variables
 CONTAINER_NAME=${CONTAINER_NAME:-"alaimos/oncoreport"}
 CONTAINER_VERSION=${CONTAINER_VERSION:-"$DEFAULT_VERSION"}
+CONTAINER_BASE_PATH=${CONTAINER_BASE_PATH:-"/support"}
 
 echo "Building container ${CONTAINER_NAME}:${CONTAINER_VERSION}"
 
@@ -37,8 +39,20 @@ if [ ! -f "/tmp/secret_drugbank" ]; then
 fi
 
 DOCKER_BUILDKIT=1 docker build \
-  --build-arg="ONCOREPORT_BRANCH=${ONCOREPORT_BRANCH}" \
-  --secret id=drugbank,src=/tmp/secret_drugbank \
-  --secret id=gltranslate,src=$GLTRANSLATE_JSON_PATH \
-  -t "${CONTAINER_NAME}:${CONTAINER_VERSION}" . &&
-  rm /tmp/secret_drugbank
+  --target=stage1_base \
+  -t oncoreport_builder_stage_1 . &&
+  docker run --rm \
+    --mount type=bind,source="/tmp/secret_drugbank",target="/run/secrets/drugbank",readonly \
+    --mount type=bind,source="${GLTRANSLATE_JSON_PATH}",target="/run/secrets/gltranslate",readonly \
+    --mount type=bind,source="${SCRIPT_PATH}",target="${CONTAINER_BASE_PATH}" \
+    --env ONCOREPORT_ORIGIN="https://github.com/knowmics-lab/oncoreport.git" \
+    --env ONCOREPORT_BRANCH="${ONCOREPORT_BRANCH}" \
+    --env BASE_PATH="${CONTAINER_BASE_PATH}" \
+    oncoreport_builder_stage_1 bash ${CONTAINER_BASE_PATH}/scripts/prepare.sh
+
+# DOCKER_BUILDKIT=1 docker build \
+#   --build-arg="ONCOREPORT_BRANCH=${ONCOREPORT_BRANCH}" \
+#   --secret id=drugbank,src=/tmp/secret_drugbank \
+#   --secret id=gltranslate,src=$GLTRANSLATE_JSON_PATH \
+#   -t "${CONTAINER_NAME}:${CONTAINER_VERSION}" . &&
+#   rm /tmp/secret_drugbank
